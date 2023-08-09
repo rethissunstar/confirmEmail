26ed9d155459afdb729d23f4a555079f1310d801
rethissunstar <rethissunstar@gmail.com>
2023-08-09 19:47:17 -0400

    updated some formatting

--- user-auth-app/src/App.js
+++ user-auth-app/src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; 
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Login from "./components/Login";
 import CreateAccount from "./components/CreateAccount";
+import "./App.css"; // Import the CSS file for styling
 
 const App = () => {
   return (
     <Router>
-      <div>
+      <div className="app-container">
         <h1>Welcome to our User Authentication System</h1>
-        <Link to="/login">Login</Link>
-        <Link to="/create">Create Account</Link>
+        <div className="button-container">
+          <Link to="/login" className="app-button">Login</Link>
+          <Link to="/create" className="app-button">Create Account</Link>
+        </div>
       </div>
-      <Routes> 
-        <Route path="/login" element={<Login />} /> 
-        <Route path="/create" element={<CreateAccount />} /> 
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/create" element={<CreateAccount />} />
       </Routes>
     </Router>
   );
